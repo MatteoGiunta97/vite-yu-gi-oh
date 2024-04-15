5b6700d86bfa714ea7c18c99185a78f1041de01e
MatteoGiunta97 <matteogiunta@outlook.it>
2024-04-15 16:36:25 +0200

    feat: add main content structure

--- src/components/AppMainContent.vue
+++ src/components/AppMainContent.vue
@@ -6,9 +6,44 @@
 
 <template>
   <!-- https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0 -->
+
+  <section class="cards-wrapper">
+    <div class="container">
+      <div class="cards-list">
+        <div class="card">temporibus perspiciatis autem minus error aut nesciunt repudiandae adipisci enim incidunt odit nam, aspernatur voluptates obcaecati.</div>
+        <div class="card">temporibus perspiciatis autem minus error aut nesciunt repudiandae adipisci enim incidunt odit nam, aspernatur voluptates obcaecati.</div>
+        <div class="card">temporibus perspiciatis autem minus error aut nesciunt repudiandae adipisci enim incidunt odit nam, aspernatur voluptates obcaecati.</div>
+        <div class="card">temporibus perspiciatis autem minus error aut nesciunt repudiandae adipisci enim incidunt odit nam, aspernatur voluptates obcaecati.</div>
+        <div class="card">temporibus perspiciatis autem minus error aut nesciunt repudiandae adipisci enim incidunt odit nam, aspernatur voluptates obcaecati.</div>
+        <div class="card">temporibus perspiciatis autem minus error aut nesciunt repudiandae adipisci enim incidunt odit nam, aspernatur voluptates obcaecati.</div>
+        <div class="card">temporibus perspiciatis autem minus error aut nesciunt repudiandae adipisci enim incidunt odit nam, aspernatur voluptates obcaecati.</div>
+        <div class="card">temporibus perspiciatis autem minus error aut nesciunt repudiandae adipisci enim incidunt odit nam, aspernatur voluptates obcaecati.</div>
+        <div class="card">temporibus perspiciatis autem minus error aut nesciunt repudiandae adipisci enim incidunt odit nam, aspernatur voluptates obcaecati.</div>
+        <div class="card">temporibus perspiciatis autem minus error aut nesciunt repudiandae adipisci enim incidunt odit nam, aspernatur voluptates obcaecati.</div>
+      </div>
+    </div>
+  </section>
+
 </template>
 
 <style scoped lang="scss">
 
+.cards-wrapper {
+  background-color: #d48f38;
+  padding: 20px;
 
+  .container {
+    background-color: white;
+
+    .cards-list {
+      display: flex;
+      flex-wrap: wrap;
+
+      .card {
+        width: calc(100% / 5);
+        padding: 5px;
+      }
+    }
+  }
+}
 </style>
